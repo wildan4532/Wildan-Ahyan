bd423a54a0e9b001256289faf279f01d53a51aba
Wildan Ahyann Siregar <wildan_prasetyo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:55:53 +0700

    Add files via upload

---
+++ TugasObjek/Guru.java
@@ -0,0 +1,13 @@
+//Object Class
+public class Guru {
+    int id;
+    String nama, mapel, alamat;
+    
+
+    public Guru(){
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+    }
+}
---
+++ TugasObjek/TesGuru.java
@@ -0,0 +1,70 @@
+public class TesGuru {
+    public static void main(String[] args) {
+        //Make Object
+        Guru buAulia = new Guru();
+        Guru buFeniliya = new Guru();
+        Guru buRia = new Guru();
+        Guru pakFalah = new Guru();
+        Guru pakBagus = new Guru();
+        
+
+
+        //Bu Aulia
+        buAulia.id = 1;
+        buAulia.nama = "Bu Aulia Mas'adah";
+        buAulia.mapel = "Guru Produktif RPL";
+        buAulia.alamat ="Malang";
+        System.out.println(buAulia.id);
+        System.out.println(buAulia.nama);
+        System.out.println(buAulia.mapel);
+        System.out.println(buAulia.alamat);
+        System.out.println(" ");
+
+
+        //Bu Feniliya
+        buFeniliya.id = 2;
+        buFeniliya.nama = "Bu Feniliya Mayrini";
+        buFeniliya.mapel = "Guru Matematika";
+        buFeniliya.alamat ="Malang";
+        System.out.println(buFeniliya.id);
+        System.out.println(buFeniliya.nama);
+        System.out.println(buFeniliya.mapel);
+        System.out.println(buFeniliya.alamat);
+        System.out.println(" ");
+        
+        //Bu Ria atau Bu rara
+        buRia.id = 3;
+        buRia.nama = "Bu Ria Cahya Kurnia";
+        buRia.mapel = "Guru PPKN";
+        buRia.alamat ="Malang";
+        System.out.println(buRia.id);
+        System.out.println(buRia.nama);
+        System.out.println(buRia.mapel);
+        System.out.println(buRia.alamat);
+        System.out.println(" ");
+
+        //Pak Falah
+        pakFalah.id = 4;
+        pakFalah.nama = "Pak Muhammad Fajrul Falah";
+        pakFalah.mapel = "Guru Seni Budaya";
+        pakFalah.alamat ="Malang";
+        System.out.println(pakFalah.id);
+        System.out.println(pakFalah.nama);
+        System.out.println(pakFalah.mapel);
+        System.out.println(pakFalah.alamat);
+        System.out.println(" ");
+
+        //Pak Bagus
+       pakBagus.id = 5;
+       pakBagus.nama = "Pak Muhammad Bagus Arifin";
+       pakBagus.mapel = "Guru Informatika";
+       pakBagus.alamat ="Malang";
+       System.out.println(pakBagus.id);
+       System.out.println(pakBagus.nama);
+       System.out.println(pakBagus.mapel);
+       System.out.println(pakBagus.alamat);
+       System.out.println(" ");
+       
+    }
+    
+}
